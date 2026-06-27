1c1c32d11329f67e1c7010756796a967310999f0
Dipak-M <punmgr577@gmail.com>
2026-06-27 14:32:28 +0545

    Initial MERN project setup

---
+++ server/app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const cookieParser = require("cookie-parser");
+
+const authRoutes = require("./routes/authRoutes");
+
+const app = express();
+
+app.use(helmet());
+app.use(morgan("dev"));
+app.use(cors());
+app.use(cookieParser());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Clothing Brand API");
+});
+
+app.use("/api/auth", authRoutes);
+
+module.exports = app;
